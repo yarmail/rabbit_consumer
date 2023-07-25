d25ae2a932733a7d94b2d5a1d46315afb2087ee6
yarmail <yarmail@yandex.ru>
2023-07-25 20:37:57 +0300

    update

--- src/main/java/com/example/rabbit_consumer/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbit_consumer/config/RabbitMQConfig.java
@@ -1,7 +1,9 @@
 package com.example.rabbit_consumer.config;
 
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -11,23 +13,28 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    @Value("${spring.rabbitmq.host}")
+    @Value("${consumer.rabbitmq.host}")
     private String host;
-    @Value("${spring.rabbitmq.virtual-host}")
+    @Value("${consumer.rabbitmq.virtual-host}")
     private String virtualHost;
-    @Value("${spring.rabbitmq.username}")
+    @Value("${consumer.rabbitmq.username}")
     private String username;
-    @Value("${spring.rabbitmq.password}")
+    @Value("${consumer.rabbitmq.password}")
     private String password;
-    @Value("${spring.rabbitmq.port}")
+    @Value("${consumer.rabbitmq.port}")
     private int port;
 
-    @Value("${spring.rabbitmq.exchange}")
+    @Value("${consumer.rabbitmq.exchange}")
     private String exchange;
-    @Value("${spring.rabbitmq.queue}")
+    @Value("${consumer.rabbitmq.queue}")
     private String queue;
-    @Value("${spring.rabbitmq.routingkey}")
+    @Value("${consumer.rabbitmq.routingkey}")
     private String routingkey;
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
 
     @Bean
     CachingConnectionFactory connectionFactory() {
@@ -37,6 +44,24 @@ public class RabbitMQConfig {
         cachingConnectionFactory.setPort(port);
         cachingConnectionFactory.setVirtualHost(virtualHost);
         return cachingConnectionFactory;
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(routingkey);
     }
 
     @Bean
