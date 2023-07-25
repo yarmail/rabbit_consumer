686fed2da0ce57214b19df193ef027274aa33803
yarmail <yarmail@yandex.ru>
2023-07-25 20:20:37 +0300

    update

--- src/main/java/com/example/rabbit_consumer/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbit_consumer/config/RabbitMQConfig.java
@@ -1,9 +1,7 @@
 package com.example.rabbit_consumer.config;
 
-import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -15,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
     @Value("${spring.rabbitmq.host}")
     private String host;
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
     @Value("${spring.rabbitmq.username}")
     private String username;
     @Value("${spring.rabbitmq.password}")
@@ -35,7 +35,7 @@ public class RabbitMQConfig {
         cachingConnectionFactory.setUsername(username);
         cachingConnectionFactory.setPassword(password);
         cachingConnectionFactory.setPort(port);
-        cachingConnectionFactory.setCacheMode(CachingConnectionFactory.CacheMode.CONNECTION);
+        cachingConnectionFactory.setVirtualHost(virtualHost);
         return cachingConnectionFactory;
     }
 
