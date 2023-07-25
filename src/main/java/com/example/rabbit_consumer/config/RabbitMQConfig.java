6e1258cf773ddac343e2066dca875f9ab9b9c90b
yarmail <yarmail@yandex.ru>
2023-07-26 02:09:33 +0300

    update

--- src/main/java/com/example/rabbit_consumer/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbit_consumer/config/RabbitMQConfig.java
@@ -1,12 +1,49 @@
 package com.example.rabbit_consumer.config;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.userExchange}")
+    private String userExchange;
+    @Value("${spring.rabbitmq.userQueue}")
+    private String userQueue;
+    @Value("${spring.rabbitmq.userRoutingKey}")
+    private String userRoutingKey;
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+        cachingConnectionFactory.setPort(port);
+        cachingConnectionFactory.setVirtualHost(virtualHost);
+        return cachingConnectionFactory;
+    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
